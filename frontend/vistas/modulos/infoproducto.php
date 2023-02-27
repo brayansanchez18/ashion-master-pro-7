1835f47194690533f96617f8330fd70564232085
Emanuel <brayan.sanchez.contacto@gmail.com>
2023-02-26 21:41:31 -0600

    se maqueto la pagina de info producto

--- frontend/vistas/modulos/infoproducto.php
+++ frontend/vistas/modulos/infoproducto.php
@@ -1,1 +1,184 @@
-<h1>INFO PRODUCTOS</h1>
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="breadcrumb__links">
+          <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+          <a href="#">Women’s </a>
+          <span>Essential structured blazer</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- Product Details Section Begin -->
+<section class="product-details spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="product__details__pic">
+                    <div class="product__details__pic__left product__thumb nice-scroll">
+                        <a class="pt active" href="#product-1">
+                            <img src="vistas/img/product/details/thumb-1.jpg" alt="">
+                        </a>
+                        <a class="pt" href="#product-2">
+                            <img src="vistas/img/product/details/thumb-2.jpg" alt="">
+                        </a>
+                        <a class="pt" href="#product-3">
+                            <img src="vistas/img/product/details/thumb-3.jpg" alt="">
+                        </a>
+                        <a class="pt" href="#product-4">
+                            <img src="vistas/img/product/details/thumb-4.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="product__details__slider__content">
+                        <div class="product__details__pic__slider owl-carousel">
+                            <img data-hash="product-1" class="product__big__img" src="vistas/img/product/details/product-1.jpg" alt="">
+                            <img data-hash="product-2" class="product__big__img" src="vistas/img/product/details/product-3.jpg" alt="">
+                            <img data-hash="product-3" class="product__big__img" src="vistas/img/product/details/product-2.jpg" alt="">
+                            <img data-hash="product-4" class="product__big__img" src="vistas/img/product/details/product-4.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="product__details__text">
+                    <h3>Essential structured blazer</h3>
+
+                    <div class="product__details__price">$ 75.0 <span>$ 83.0</span></div>
+                    <p>Nemo enim ipsam voluptatem quia aspernatur aut odit aut loret fugit, sed quia consequuntur
+                    magni lores eos qui ratione voluptatem sequi nesciunt.</p>
+                    <hr>
+                    <div class="col-12 h4 text-muted"><strong>123</strong> Unidades Disponibles</div>
+                    <hr>
+                    <div class="product__details__button">
+                        <a href="#" class="cart-btn"><span class="icon_bag_alt"></span> Agregar al carrito</a>
+                        <ul>
+                            <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__details__widget d-flex">
+                      <div class="col-md-6">
+                        <select class="form-control seleccionarDetalle" id="seleccionarTalla">
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                        </select>
+                      </div>
+                      <br>
+                      <div class="col-md-6">
+                        <select class="form-control seleccionarDetalle" id="seleccionarTalla">
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                          <option value="">Talla</option>
+                        </select>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="product__details__tab">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">Description</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tabs-1" role="tabpanel">
+                            <h6>Description</h6>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed
+                                quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt loret.
+                                Neque porro lorem quisquam est, qui dolorem ipsum quia dolor si. Nemo enim ipsam
+                                voluptatem quia voluptas sit aspernatur aut odit aut loret fugit, sed quia ipsu
+                                consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nulla
+                            consequat massa quis enim.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+                                dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                            quis, sem.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="related__title">
+                    <h5>RELATED PRODUCTS</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="vistas/img/product/related/rp-1.jpg">
+                        <div class="label new">New</div>
+                        <ul class="product__hover">
+                            <li><a href="vistas/img/product/related/rp-1.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                            <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                            <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__item__text">
+                        <h6><a href="#">Buttons tweed blazer</a></h6>
+                        <br>
+                        <div class="product__price">$ 59.0</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="vistas/img/product/related/rp-2.jpg">
+                        <ul class="product__hover">
+                            <li><a href="vistas/img/product/related/rp-2.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                            <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                            <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__item__text">
+                        <h6><a href="#">Flowy striped skirt</a></h6>
+                        <br>
+                        <div class="product__price">$ 49.0</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="vistas/img/product/related/rp-3.jpg">
+                        <div class="label stockout">out of stock</div>
+                        <ul class="product__hover">
+                            <li><a href="vistas/img/product/related/rp-3.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                            <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                            <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__item__text">
+                        <h6><a href="#">Cotton T-Shirt</a></h6>
+                        <br>
+                        <div class="product__price">$ 59.0</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="vistas/img/product/related/rp-4.jpg">
+                        <ul class="product__hover">
+                            <li><a href="vjistas/img/product/related/rp-4.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                            <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                            <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                        </ul>
+                    </div>
+                    <div class="product__item__text">
+                        <h6><a href="#">Slim striped pocket shirt</a></h6>
+                        <br>
+                        <div class="product__price">$ 59.0</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Product Details Section End -->
